ac8e2fa91f34747e6542338383b111c19f710c33
Anton Smolyanyy <antonsmo271@gmail.com>
2024-11-05 22:30:09 -0700

    Improved algorithm efficiency

--- lu_decomposition.py
+++ lu_decomposition.py
@@ -8,8 +8,6 @@ vectorB = np.array([[13,-10,22]], dtype=float)
 matrixC = np.array([[3,-7,-2,2], [-3,5,1,0], [6,-4,0,-5], [-9,5,-5,12]])
 vectorD = np.array([[-9,5,7,11]])
 
-matrixP = np.array([[0,1,0], [1,0,0], [0,0,1]])
-
 def lu_factorization_pivot_at_00(matrix):
 
     """
@@ -84,19 +82,14 @@ def solve_x_by_lu(matrix, given_vector):
     vectorY = np.zeros((matrixL.shape[0], 1))
     for position in range(vectorY.shape[0]):
 
-        vectorY[position,0] = given_vector[position,0]
-        for i in range(position,0,-1):
-            vectorY[position,0] -= matrixL[position, i-1] * vectorY[i-1,0]
+        vectorY[position,0] = given_vector[position,0] - matrixL[position, :position] @ vectorY[:position, 0]
     
     #backwards substitution
     vectorX = np.zeros((matrixL.shape[0], 1))
     for position in range(vectorY.shape[0]-1,-1,-1):
-
-        vectorX[position,0] = vectorY[position,0] 
-        for i in range(position,vectorY.shape[0]-1):
-            vectorX[position,0] -= matrixU[position, i+1] * vectorX[i+1,0]
-        vectorX[position,0] /= matrixU[position,position]
-
+        
+        vectorX[position,0] = (vectorY[position,0] - (matrixU[position, position:] @ vectorX[position:,0])) / matrixU[position,position]
+        
     return vectorX
 
 
