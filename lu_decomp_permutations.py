b393c7521cb13a612499e14f541b226c40793fe9
Anton Smolyanyy <antonsmo271@gmail.com>
2024-11-07 14:28:20 -0700

    Implemented LU factorization with permutations

--- lu_decomp_permutations.py
+++ lu_decomp_permutations.py
@@ -1,21 +1,22 @@
 #LU Decomposition with Permutations
 import numpy as np
 import copy
+import scipy
 
-matrixA = np.array([[-3,-3,-2.], [3,0,2], [-6,6,-8]], dtype=float)
+matrixA = np.array([[2,4,2], [4,-10,2], [1,2,4]])
 vectorB = np.array([[13,-10,22]], dtype=float)
 
-matrixC = np.array([[3,-7,-2,2], [-3,5,1,0], [6,-4,0,-5], [-9,5,-5,12]])
+matrixC = np.array([[0,1,1], [1,3,7], [2,4,8]])
 vectorD = np.array([[-9,5,7,11]])
 
-matrixE = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]])
+matrixE = np.array([[2, 5, 8, 7], [5, 2, 2, 8], [7, 5, 6, 6], [5, 4, 4, 8]])
 matrixP = np.array([[0,0,0,1], [0,0,1,0], [1,0,0,0], [0,1,0,0]])
-vectorP = np.array([[4], [3], [1], [2]])
+vectorP = np.array([[4,3,1,2]])
 
 def lu_factorization_pivot_anywhere(matrix):
 
     """
-    Finds the LU decomposition of a square array that does not require permutation (the pivot element is at [0,0]).
+    Finds the LU decomposition of a square array that may or may not require permutation(s).
 
     Parameters
     ----------
@@ -26,6 +27,7 @@ def lu_factorization_pivot_anywhere(matrix):
     -------
     tuple of np.ndarray
         A tuple containing:
+        - total_perm_vector (nd.ndarray): nx1 vector that holds the permutation information as row indices
         - l (np.ndarray): Lower triangular array with 1's on diagonal (shape nxn).
         - u (np.ndarray): Upper triangular array (shape nxn).
 
@@ -43,14 +45,31 @@ def lu_factorization_pivot_anywhere(matrix):
 
     l = np.identity(size)
     u = np.zeros((size, size))
+    total_perm_vector = np.arange(0, size).reshape(size, 1)
 
     for step in range(size):
+
+        perm_vector = np.arange(1, size + 1).reshape(1, size)
+        largest = currMatrix[step,step]
+        index_largest = step
+        for index in range(step, matrix.shape[0]):
+            if (abs(currMatrix[index,step]) > largest):
+                index_largest = index
+
+        if (index_largest != step):
+            pivot_row = perm_vector[0,index_largest]
+            for row in range(perm_vector.shape[1]-2,-1,-1):
+                perm_vector[0,row+1] = perm_vector[0,row]
+            perm_vector[0,0] = pivot_row
+
+        currMatrix = generate_perm_matrix(perm_vector.T) @ currMatrix
+        total_perm_vector = generate_perm_matrix(perm_vector.T) @ total_perm_vector
 
         l[step+1:,step] = currMatrix[step+1:,step] / currMatrix[step, step]
         u[step] = currMatrix[step]
         currMatrix = np.subtract(currMatrix, l[:,step].reshape(size,1) @ u[step].reshape(1,size))
 
-    return (l, u)
+    return (total_perm_vector,l, u)
 
 def solve_x_by_lu(matrix, given_vector):
 
@@ -96,7 +115,7 @@ def solve_x_by_lu(matrix, given_vector):
         
     return vectorX
 
-def permute_matrix(perm_matrix, matrix):
+
 
     if (perm_matrix.shape[1] > 1):
         return perm_matrix @ matrix
@@ -118,7 +137,10 @@ def generate_perm_matrix(vector):
     return perm_matrix
 
 #print(np.allclose(solve_x_by_lu(matrixA, vectorB.T), np.linalg.solve(matrixA, vectorB.T)))
-#print(np.allclose(solve_x_by_lu(matrixC, vectorD.T), np.linalg.solve(matrixC, vectorD.T)))
 
-#print(permute_matrix(matrixP, matrixE))
-print(generate_perm_matrix(vectorP))
+#print(permute_matrix(matrixP.T, matrixE))
+#print(lu_factorization_pivot_anywhere(matrixC))
+
+#print(lu_factorization_pivot_anywhere(matrixE)[1] @ lu_factorization_pivot_anywhere(matrixE)[2])
+#print(scipy.linalg.lu(matrixE)[1] @ scipy.linalg.lu(matrixE)[2])
+#print(scipy.linalg.lu(matrixC)[0])
